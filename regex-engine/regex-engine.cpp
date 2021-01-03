5e6f4b551d7908e14cd9d77452831bb0687f774d
nalidzhik <nazzalidzhikk@gmail.com>
2021-01-03 12:44:43 +0200

    Add simple literal matching

---
+++ regex-engine/regex-engine.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+bool doesUnitMatch(string& regex, string& input)
+{
+	return regex[0] == input[0];
+}
+
+bool matchRegex(string& regex, string& input)
+{
+	if (regex.size() == 0)
+	{
+		return true;
+	}
+
+	if (doesUnitMatch(regex, input))
+	{
+		string regexRest = regex.substr(1);
+		string inputRest = input.substr(1);
+		return matchRegex(regexRest, inputRest);
+	}
+
+	return false;
+}
+
+bool match(string& regex, string& input)
+{
+	int matchPos = 0;
+	bool matched = false;
+	while (!matched && matchPos < input.size())
+	{
+		string inputRest = input.substr(matchPos);
+		matched = matchRegex(regex, inputRest);
+		if (matched)
+		{
+			return true;
+		}
+
+		matchPos += 1;
+	}
+
+	return false;
+}
+
+int main()
+{
+	string regex = "abc";
+	string input = "hello, abc foo!";
+
+	cout << match(regex, input) << endl;
+
+	return 0;
+}
